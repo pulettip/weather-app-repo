57d6f6ca4c1c63d6bcc301b410f0958e894223cf
Pablo Puletti <pulettip@gmail.com>
2025-05-02 23:12:03 +0100

    removed console.log

--- packages/backend/src/controllers/weatherService.ts
+++ packages/backend/src/controllers/weatherService.ts
@@ -4,6 +4,5 @@ import { calculateScores, getCityCoordinates } from './scoreUtils';
 export async function getWeatherScores(city: string) {
   const { lat, lon } = await getCityCoordinates(city);
   const response = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max&timezone=auto`);
-  console.log('Weather data:', response.data);
   return calculateScores(response.data);
 }
